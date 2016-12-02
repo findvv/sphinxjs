b40e4dbe5fafc4a663754bbb64c7550be2d8216e
ybbjegj <guomilo@gmail.com>
2016-12-02 12:12:05 +0800

    fix:修复logo缺少n的问题

--- logo.js
+++ logo.js
@@ -6,13 +6,13 @@ module.exports = function (version) {
     let lines = [],
         B = _.isOSX() ? '▒' : '/';
 
-    lines.push(chalk.red(`  ${B}${B}${B}${B}  `) + chalk.yellow(`${B}${B}${B}${B}   `) + chalk.red(`${B}   ${B}   `) + chalk.green(`${B}${B}${B}  `) + chalk.cyan(`${B}   ${B}`));
-    lines.push(chalk.red(` ${B}      `) + chalk.yellow(`${B}   ${B}  `) + chalk.red(`${B}   ${B}   `) + chalk.green(` ${B}   `) + chalk.cyan(`${B}   ${B}`));
-    lines.push(chalk.red(` ${B}      `) + chalk.yellow(`${B}   ${B}  `) + chalk.red(`${B}   ${B}   `) + chalk.green(` ${B}   `) + chalk.cyan(` ${B} ${B} `));
-    lines.push(chalk.red(`  ${B}${B}${B}   `) + chalk.yellow(`${B}${B}${B}${B}   `) + chalk.red(`${B}${B}${B}${B}${B}   `) + chalk.green(` ${B}   `) + chalk.cyan(`  ${B}  `));
-    lines.push(chalk.red(`     ${B}  `) + chalk.yellow(`${B}      `) + chalk.red(`${B}   ${B}   `) + chalk.green(` ${B}   `) + chalk.cyan(` ${B} ${B} `));
-    lines.push(chalk.red(`     ${B}  `) + chalk.yellow(`${B}      `) + chalk.red(`${B}   ${B}   `) + chalk.green(` ${B}   `) + chalk.cyan(`${B}   ${B}`));
-    lines.push(chalk.red(` ${B}${B}${B}${B}   `) + chalk.yellow(`${B}      `) + chalk.red(`${B}   ${B}   `) + chalk.green(`${B}${B}${B}  `) + chalk.cyan(`${B}   ${B}`));
+    lines.push(chalk.red(`  ${B}${B}${B}${B}  `) + chalk.yellow(`${B}${B}${B}${B}   `) + chalk.red(`${B}   ${B}   `) + chalk.green(`${B}${B}${B}  `) + chalk.red(`${B}   ${B}  `) + chalk.cyan(`${B}   ${B}`));
+    lines.push(chalk.red(` ${B}      `) + chalk.yellow(`${B}   ${B}  `) + chalk.red(`${B}   ${B}   `) + chalk.green(` ${B}   `) + chalk.red(`${B}   ${B}  `) + chalk.cyan(`${B}   ${B}`));
+    lines.push(chalk.red(` ${B}      `) + chalk.yellow(`${B}   ${B}  `) + chalk.red(`${B}   ${B}   `) + chalk.green(` ${B}   `) + chalk.red(`${B}${B}  ${B}  `) + chalk.cyan(` ${B} ${B} `));
+    lines.push(chalk.red(`  ${B}${B}${B}   `) + chalk.yellow(`${B}${B}${B}${B}   `) + chalk.red(`${B}${B}${B}${B}${B}   `) + chalk.green(` ${B}   `) + chalk.red(`${B} ${B} ${B}  `) + chalk.cyan(`  ${B}  `));
+    lines.push(chalk.red(`     ${B}  `) + chalk.yellow(`${B}      `) + chalk.red(`${B}   ${B}   `) + chalk.green(` ${B}   `) + chalk.red(`${B}  ${B}${B}  `) + chalk.cyan(` ${B} ${B} `));
+    lines.push(chalk.red(`     ${B}  `) + chalk.yellow(`${B}      `) + chalk.red(`${B}   ${B}   `) + chalk.green(` ${B}   `) + chalk.red(`${B}   ${B}  `) + chalk.cyan(`${B}   ${B}`));
+    lines.push(chalk.red(` ${B}${B}${B}${B}   `) + chalk.yellow(`${B}      `) + chalk.red(`${B}   ${B}   `) + chalk.green(`${B}${B}${B}  `) + chalk.red(`${B}   ${B}  `) + chalk.cyan(`${B}   ${B}`));
     console.log('\n');
     version && console.log(`v${version}\n`);
     console.log(lines.join('\n'));
